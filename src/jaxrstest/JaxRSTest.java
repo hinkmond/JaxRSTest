371d6efd2b19aaf9fb11fb4c7f49d22862f42acc
Hinkmond Wong <hiwong@linkedin.com>
2017-07-27 17:54:58 -0700

    First commit

---
+++ src/jaxrstest/JaxRSTest.java
@@ -0,0 +1,34 @@
+package jaxrstest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import org.glassfish.jersey.server.ResourceConfig;
+
+
+@ApplicationPath("rest")
+public class JaxRSTest extends ResourceConfig {
+
+  public JaxRSTest() {
+    packages("jaxrstest");
+  }
+
+  @Path("hello")
+  public static class Hello {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String get() {
+      return "get: Hello world from TEXT_PLAIN!";
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public String getMessage() {
+      return "getMessage: Hello world from TEXT_HTML!";
+    }
+  }
+
+}
